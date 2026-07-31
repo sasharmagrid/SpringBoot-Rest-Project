2f6de007639e3e9f18458f882d29d01fd8c76b84
sasharma <sasharma@griddynamics.com>
2026-07-31 11:02:43 +0530

    Made URL Request DTO

---
+++ src/main/java/com/example/demo/dto/ShortenUrlRequest.java
@@ -0,0 +1,14 @@
+package com.example.demo.dto;
+
+public class ShortenUrlRequest {
+
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
